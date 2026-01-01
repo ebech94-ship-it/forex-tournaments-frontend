20bec5b69b8e1975aeaa0f91b8a7188a63e8b00d
Ekoko Bechem <ekokobechem09@gmail.com>
2026-01-01 21:47:04 +0100

    chore: unify Expo config into app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -4,9 +4,58 @@ export default {
   expo: {
     name: "Forex Tournaments Arena",
     slug: "forex-tournaments-arena",
+    owner: "ebeh",
+
+    version: "1.0.0",
+    orientation: "default",
+    scheme: "forextournamentsarena",
+
+    icon: "./assets/images/icon.png",
+    userInterfaceStyle: "automatic",
+
+    newArchEnabled: true,
+
+    ios: {
+      supportsTablet: true,
+    },
 
     android: {
       package: "com.ebeh.forextournamentsarena",
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/icon.png",
+        monochromeImage: "./assets/images/android-icon-monochrome.png",
+        backgroundColor: "#E6F4FE",
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+    },
+
+    web: {
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+
+    plugins: [
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/me.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+          dark: {
+            backgroundColor: "#000000",
+          },
+        },
+      ],
+      "expo-secure-store",
+      "expo-web-browser",
+      "expo-localization",
+    ],
+
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
     },
 
     extra: {
@@ -15,6 +64,12 @@ export default {
       googleWebClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
       googleAndroidClientId: process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID,
       googleIosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID,
+
+      router: {},
+
+      eas: {
+        projectId: "9aeadda0-8d0b-4833-9971-158baac2d2ad",
+      },
     },
   },
 };
